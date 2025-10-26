4ac5589569510312fd9977fc3e09a468c14e0a93
Ellis Breen <1263585+griels@users.noreply.github.com>
2025-10-26 16:10:30 +0000

    Add full CBOR decode support

--- src/deserialize/deserializer.rs
+++ src/deserialize/deserializer.rs
@@ -2,27 +2,43 @@
 
 use crate::deserialize::utf8::read_input_to_buf;
 use crate::deserialize::DeserializeError;
+use crate::opt::{Opt, CBOR};
 use crate::typeref::EMPTY_UNICODE;
 use core::ptr::NonNull;
 
 pub(crate) fn deserialize(
     ptr: *mut pyo3_ffi::PyObject,
+    opts: Option<Opt>,
 ) -> Result<NonNull<pyo3_ffi::PyObject>, DeserializeError<'static>> {
     debug_assert!(ffi!(Py_REFCNT(ptr)) >= 1);
     let buffer = read_input_to_buf(ptr)?;
     debug_assert!(!buffer.is_empty());
-
-    if unlikely!(buffer.len() == 2) {
-        if buffer == b"[]" {
-            return Ok(nonnull!(ffi!(PyList_New(0))));
-        } else if buffer == b"{}" {
-            return Ok(nonnull!(ffi!(PyDict_New())));
-        } else if buffer == b"\"\"" {
-            unsafe { return Ok(nonnull!(use_immortal!(EMPTY_UNICODE))) }
+    if  opt_enabled!(opts.unwrap_or(CBOR), 0){
+        #[cfg(not(feature = "yyjson"))]
+        {
+            crate::deserialize::backend::deserialize_cbor(str::from_utf8(buffer).unwrap())
+        }
+        #[cfg(feature = "yyjson")]
+        {
+            // Since we don't have deserialize_cbor with yyjson feature,
+            // we'll fall back to the regular deserialize
+            let buffer_str = unsafe { core::str::from_utf8_unchecked(buffer) };
+            crate::deserialize::backend::deserialize(buffer_str)
         }
     }
+    else {
+        if unlikely!(buffer.len() == 2) {
+            if buffer == b"[]" {
+                return Ok(nonnull!(ffi!(PyList_New(0))));
+            } else if buffer == b"{}" {
+                return Ok(nonnull!(ffi!(PyDict_New())));
+            } else if buffer == b"\"\"" {
+                unsafe { return Ok(nonnull!(use_immortal!(EMPTY_UNICODE))) }
+            }
+        }
+        let buffer_str = unsafe { core::str::from_utf8_unchecked(buffer) };
 
-    let buffer_str = unsafe { core::str::from_utf8_unchecked(buffer) };
+        crate::deserialize::backend::deserialize(buffer_str)
+    }
 
-    crate::deserialize::backend::deserialize(buffer_str)
 }
